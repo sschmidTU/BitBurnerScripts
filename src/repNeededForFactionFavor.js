bdd7d28cdc9f1861bfb44b62b31937df22879a83
sschmidTU <33069673+sschmidTU@users.noreply.github.com>
2018-05-31 00:10:52 +0200

    Create repNeededForFactionFavor.js

---
+++ src/repNeededForFactionFavor.js
@@ -0,0 +1,39 @@
+/*
+usage: run repNeededForFactionFavor.js favorTarget currentFavor
+    returns how much reputation you need at maximum to reach the faction favor favorTarget, given your current favor
+    (this is off by the reputationRollOver from last augmentation, if you already have favor)
+    (note that company favor uses different constants)
+
+formula adapted from (Faction.js/getFavorGain):
+    https://github.com/danielyxie/bitburner/blob/master/src/Faction.js
+    
+    also available as netscript 1.0 script (running in Firefox)
+    https://github.com/sschmidTU/BitBurnerScripts/
+    @author sschmidTU
+*/
+
+function repNeededForFactionFavor(targetFavor,currentFavor) {
+    
+    let favorGain = 0;
+    let rep = 0;
+    
+    let FactionReputationToFavorBase = 500;
+    let FactionReputationToFavorMult = 1.02;
+    
+    let reqdRep = FactionReputationToFavorBase *
+                      Math.pow(FactionReputationToFavorMult, currentFavor);
+    while (currentFavor + favorGain < targetFavor) {
+        rep += reqdRep;
+        ++favorGain;
+        reqdRep *= FactionReputationToFavorMult;
+    }
+    
+    return rep;
+}
+
+export async function main(ns) {
+    let targetFavor = ns.args[0];
+    let currentFavor = ns.args[1];
+    
+    ns.tprint('required rep: ' + repNeededForFavor(targetFavor, currentFavor));
+}
