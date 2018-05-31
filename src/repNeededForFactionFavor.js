a6bf25a1245f0e85557ba2a251585922a5eb74ba
sschmidTU <33069673+sschmidTU@users.noreply.github.com>
2018-05-31 11:06:33 +0200

    repNeededForFavor is now accurate and simplified.

--- src/repNeededForFactionFavor.js
+++ src/repNeededForFactionFavor.js
@@ -1,10 +1,9 @@
 /*
-usage: run repNeededForFactionFavor.js favorTarget currentFavor
-    returns how much reputation you need at maximum to reach the faction favor favorTarget, given your current favor
-    (this is off by the reputationRollOver from last augmentation, if you already have favor)
-    (note that company favor uses different constants)
+usage: run repNeededForFavor.js favorTarget
+    returns how much reputation you need in total with a faction or company to reach the favor favorTarget.
+    (as of v0.37.1, the constans are the same for factions and companies)
 
-formula adapted from (Faction.js/getFavorGain):
+formula adapted from Faction.js/getFavorGain and Company.js/getFavorGain and Constants.js:
     https://github.com/danielyxie/bitburner/blob/master/src/Faction.js
     
     also available as netscript 1.0 script (running in Firefox)
@@ -12,20 +11,20 @@ formula adapted from (Faction.js/getFavorGain):
     @author sschmidTU
 */
 
-function repNeededForFactionFavor(targetFavor,currentFavor) {  
+function repNeededForFavor(targetFavor) {
+    
     let favorGain = 0;
     let rep = 0;
     
-    let FactionReputationToFavorBase = 500;
-    let FactionReputationToFavorMult = 1.02;
+    let ReputationToFavorBase = 500;
+    let ReputationToFavorMult = 1.02;
     
-    let reqdRep = FactionReputationToFavorBase *
-                      Math.pow(FactionReputationToFavorMult, currentFavor);
-    
+    let reqdRep = ReputationToFavorBase;
+    let currentFavor = 0;
     while (currentFavor + favorGain < targetFavor) {
         rep += reqdRep;
         ++favorGain;
-        reqdRep *= FactionReputationToFavorMult;
+        reqdRep *= ReputationToFavorMult;
     }
     
     return rep;
@@ -33,7 +32,10 @@ function repNeededForFactionFavor(targetFavor,currentFavor) {
 
 export async function main(ns) {
     let targetFavor = ns.args[0];
-    let currentFavor = ns.args[1];
+    let currentRep = ns.args[1];
     
-    ns.tprint('required rep: ' + repNeededForFactionFavor(targetFavor, currentFavor));
+    let repNeeded = repNeededForFavor(targetFavor);
+    
+    ns.tprint('you need ' + repNeeded.toLocaleString() + ' total reputation with a faction or company'
+        + ' to get to ' + targetFavor + ' favor.');
 }
